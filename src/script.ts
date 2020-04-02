cb38f1fe72247834d9616d41acf6e4326fd4a7cf
Dominique Müller <dominique.m.mueller@gmail.com>
2020-04-02 15:01:52 +0200

    Get a simple version working with just subscribe() and unsubscribe() using comlink

--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,13 @@
 import ScriptWebWorker from "./script.worker";
+import { Observable, Observer, fromEvent, Subject, Subscription } from "rxjs";
+import { wrap, Remote, proxy } from "comlink";
 
 console.log("Script is running.");
 
 const main = async () => {
   // Instantiate worker
   const worker: Worker = new ScriptWebWorker();
+  const workerWrapper: any = wrap(worker);
 
   // Wait for worker to be ready
   await new Promise(resolve => {
@@ -19,7 +22,27 @@ const main = async () => {
     );
   });
 
-  console.log("Worker is ready.", worker);
+  console.log("Worker is ready.", worker, workerWrapper);
+
+  const subscriber = (count: number) => {
+    console.log("Counter:", count);
+  };
+  const unsubscribe = await workerWrapper.subscribe(proxy(subscriber));
+
+  setTimeout(async () => {
+    const subscriber2 = (count: number) => {
+      console.log("Counter:", count);
+    };
+    const unsubscribe2 = await workerWrapper.subscribe(proxy(subscriber2));
+
+    setTimeout(() => {
+      unsubscribe2();
+    }, 5000);
+  }, 2000);
+
+  setTimeout(() => {
+    unsubscribe();
+  }, 5000);
 };
 
 main();
--- src/script.worker.ts
+++ src/script.worker.ts
@@ -1,6 +1,37 @@
 declare const self: Worker;
 export default {} as typeof Worker & { new (): Worker };
 
-console.log("WebWorker script is running.");
+import { expose, proxy } from "comlink";
+import { interval } from "rxjs";
+import { tap, share } from "rxjs/operators";
 
+console.log("[WORKER] Script is running.");
+
+// Announce readiness
 self.postMessage("READY");
+
+const stream = interval(1000).pipe(
+  tap(() => {
+    console.log("[WORKER] Interval emitted.");
+  })
+  share(),
+);
+
+// stream.subscribe(number => {
+//   console.log(number);
+// });
+
+const api = {
+  subscribe: (callback: any) => {
+    const callbackProxy = (...args: any) => {
+      callback(args);
+    };
+    const subscription = stream.subscribe(callbackProxy);
+    const unsubscribeProxy = () => {
+      return subscription.unsubscribe();
+    };
+    return proxy(unsubscribeProxy);
+  }
+};
+
+expose(api, self);
